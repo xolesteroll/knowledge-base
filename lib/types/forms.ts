184b040987854ce233882a3247ac3c6d813f9aa3
Anton <coffeeenow@gmail.com>
2025-10-30 15:01:28 +0200

    fixed type eror

--- lib/types/forms.ts
+++ lib/types/forms.ts
@@ -2,5 +2,5 @@ import { lessons, categories } from "@/db/schema"
 
 export interface CreateLessonFormProps {
     // formData: typeof lessons.$inferInsert;
-    categories: [typeof categories.$inferInsert];
+    categories: typeof categories.$inferSelect[];
 }
