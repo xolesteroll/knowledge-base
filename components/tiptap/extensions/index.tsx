6ccb56add32a11ef6e549a690afc7c4b2b898d8a
Anton <coffeeenow@gmail.com>
2025-11-07 11:57:50 +0200

    hotfix

--- components/tiptap/extensions/index.tsx
+++ components/tiptap/extensions/index.tsx
@@ -1,21 +1,14 @@
-import StarterKit from '@tiptap/starter-kit'
-import BulletList from '@tiptap/extension-bullet-list'
-import BoldText from '@tiptap/extension-bold'
-import Paragraph from '@tiptap/extension-paragraph'
-import Document from '@tiptap/extension-document'
-import Text from '@tiptap/extension-text'
-import ItalicText from '@tiptap/extension-italic'
-import Heading from '@tiptap/extension-heading'
+export { default as StarterKit } from '@tiptap/starter-kit'
+export { default as BulletList } from '@tiptap/extension-bullet-list'
+export { default as BoldText } from '@tiptap/extension-bold'
+export { default as Document } from '@tiptap/extension-document'
+export { default as Paragraph } from '@tiptap/extension-paragraph'
+export { default as Text } from '@tiptap/extension-text'
+export { default as ItalicText } from '@tiptap/extension-italic'
+import { Heading } from '@tiptap/extension-heading'
 
 const myHeading = Heading.configure({
     levels: [1, 2, 3],
 })
 
 export { myHeading as Heading }
-export { BoldText }
-export { ItalicText }
-export { BulletList }
-export { Paragraph }
-export { Document }
-export { Text }
-export { StarterKit }
